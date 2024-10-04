e5aeeb2243e2920e54988a8be9c9e85c3de7e481
Jnnsx <rebikj00@jcu.cz>
2024-10-04 17:20:49 +0000

    Sude/Liche

--- first.py
+++ first.py
@@ -1,7 +1,9 @@
 def dopln_cislo(cislo):
     pass
-    If čis
+cis = int(input("zadej číslo:"))
+dopln_cislo(cis)
 
-čis = input("zadej číslo:")
-dopln_cislo(čis)
-    
+if cis % 2 == 0:
+    print("číslo je sudé")
+else:
+    print("číslo je liché")
