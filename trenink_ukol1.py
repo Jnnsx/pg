4c28e022af8f0bc999c2dc923a5f8f6c7b92df91
Jnnsx <rebikj00@jcu.cz>
2025-01-14 14:44:17 +0000

    efe

---
+++ trenink_ukol1.py
@@ -0,0 +1,11 @@
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        print(f"Číslo {cislo} je sudé")
+    else:
+        print(f"Číslo {cislo} je liché")
+
+
+
+
+sudy_nebo_lichy(8)
+sudy_nebo_lichy(7)
